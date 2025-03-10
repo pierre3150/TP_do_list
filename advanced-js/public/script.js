dfa4486b8f596044545b71f0665d68896e74ab5c
pierre3150 <pierre.parain315@gmail.com>
2025-03-10 16:33:56 +0100

    pskdnvkpsn

--- advanced-js/public/script.js
+++ advanced-js/public/script.js
@@ -2,3 +2,6 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(React.createElement("h1", {}, "Hello World !")); 
+
+let title = <h1>Cours React</h1>; 
+console.log(title); 
--- react-learn/src/App.jsx
+++ react-learn/src/App.jsx
@@ -1,4 +1,30 @@
-function App() { 
-  return "<h1>Hello World !</h1>" 
-  } 
-  export default App 
+import { createElement } from "react"
+import app from "./app.module.css"
+import "./style.css"
+
+
+function App() {
+    let firstname = "John";
+    return (
+        <>
+            <div class="container">
+                <h1 className = {app['textGreen']}>ToDo Liste</h1>
+                <div class="input-section">
+                    <button id="addTask">Ajouter</button>
+                </div>
+                <div class="filter-section">
+                    <button data-filter="all" class="filter-btn">Toutes</button>
+                    <button data-filter="completed" class="filter-btn">Terminées</button>
+                    <button data-filter="pending" class="filter-btn">En cours</button>
+                </div>
+                <div>
+                    <input type = "checkbox"></input>
+                    <p>nom de la task</p>
+                    <button><i class="fa-solid fa-check"></i></button>
+
+                </div>
+            </div>
+        </>
+    )
+}
+export default App
